8b618bf30eb255224e88a590d5aca2a53da40960
Frank Mühlschlegel <frank@fluentchange.com>
2019-03-19 16:26:34 +1100

    first release

---
+++ AzureDevopsBuildIdIncrementer/AzureDevOpsClient.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.TeamFoundation.Build.WebApi;
+using Microsoft.TeamFoundation.Core.WebApi;
+using Microsoft.VisualStudio.Services.Client;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AzureDevopsBuildIdIncrementer
+{
+    class AzureDevOpsClient
+    { 
+
+        public Uri Endpoint { get; private set; }
+        public TeamProjectReference SelectedProject { get; private set; }
+        public BuildDefinitionReference DummyBuildDef { get; private set; }
+
+        private VssConnection azureDevopsConnection;
+        private BuildHttpClient buildClient;
+        private ProjectHttpClient projectClient;
+        private int maxBuildId; 
+
+        public AzureDevOpsClient(string organizationName)
+        {
+            organizationName = organizationName.Trim();
+            Endpoint = new Uri(@"https://dev.azure.com/" + organizationName + "/");
+            var creds = new VssClientCredentials(new WindowsCredential(false), new VssFederatedCredential(true), CredentialPromptType.PromptIfNeeded);
+            azureDevopsConnection = new VssConnection(Endpoint, creds);
+            azureDevopsConnection.ConnectAsync().Wait();
+
+            buildClient = azureDevopsConnection.GetClient<BuildHttpClient>();
+            projectClient = azureDevopsConnection.GetClient<ProjectHttpClient>();
+        }
+
+        public List<TeamProjectReference> Projects
+        {
+            get
+            {    
+                return projectClient.GetProjects().Result.ToList();
+            }           
+        }
+
+        public void Select(int index)
+        {
+            SelectedProject = Projects[index];
+        }
+
+        public void SetMaxBuildId(int maxBuildId)
+        {
+            this.maxBuildId = maxBuildId;
+        }
+        
+        public static string DummyDefName = "Dummy Build for Incrementing BuildId";
+        public void CloneBuildDefinitionIfNeeded()
+        {
+            var definitions = buildClient.GetDefinitionsAsync(project: SelectedProject.Name).Result;
+            var firstDef = definitions.First();
+            DummyBuildDef = definitions.FirstOrDefault(b => b.Name== DummyDefName) ;
+            
+            if (DummyBuildDef == null) // clone needs to be created
+            {
+                var cloneDef = buildClient.GetDefinitionAsync(SelectedProject.Id, firstDef.Id).Result;
+                cloneDef.Name = DummyDefName;
+                var cloneResult = buildClient.CreateDefinitionAsync(cloneDef, SelectedProject.Id, firstDef.Id).Result;
+
+                var newDefinitions = buildClient.GetDefinitionsAsync(project: SelectedProject.Name).Result;
+                DummyBuildDef = newDefinitions.First(b => b.Name == DummyDefName);      
+            }       
+        }
+
+        public void DeleteBuildDefinitinIfPossible()
+        {
+            var definitions = buildClient.GetDefinitionsAsync(project: SelectedProject.Name).Result;
+            DummyBuildDef = definitions.FirstOrDefault(b => b.Name == DummyDefName);
+
+            if (DummyBuildDef != null) // clone needs to be deleted
+            {             
+                var cloneResult = buildClient.DeleteDefinitionAsync(SelectedProject.Id, DummyBuildDef.Id);
+            }
+        }
+
+        public int LastBuildIdWithOutDeleted
+        {
+            get
+            {              
+                var builds = buildClient.GetBuildsAsync(project: SelectedProject.Name).Result;
+                return builds.Max(b => b.Id);
+            }
+        }
+
+        public List<int> MakeDummyIncrementBuildsUntilMax()
+        {
+            var idsIncremented = new List<int>();
+            int lastBuildId = LastBuildIdWithOutDeleted;
+            if (maxBuildId <= LastBuildIdWithOutDeleted) throw new Exception("max buildid not higher then last possible id");
+            do
+            {
+                lastBuildId = MakeNewDummyIncrementBuild();
+                idsIncremented.Add(lastBuildId);
+
+            } while (lastBuildId < maxBuildId);
+            return idsIncremented;
+        }
+
+        public int MakeNewDummyIncrementBuild()
+        {          
+            //queue
+            var queueResult = buildClient.QueueBuildAsync(new Build
+            {
+                Definition = new DefinitionReference
+                {
+                    Id = DummyBuildDef.Id
+                },
+                //SourceBranch = "master_android",
+                Project = DummyBuildDef.Project,
+                
+            }).Result;
+                      
+            // cancel
+            queueResult.Status = BuildStatus.Cancelling;
+            var cancelResult = buildClient.UpdateBuildAsync(queueResult, queueResult.Id).Result;
+
+            // deleted
+            buildClient.DeleteBuildAsync(SelectedProject.Id,cancelResult.Id).Wait();
+
+            return cancelResult.Id;
+        }
+    }
+}
---
+++ AzureDevopsBuildIdIncrementer/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace AzureDevopsBuildIdIncrementer
+{
+    class Program
+    {
+
+        private static AzureDevOpsClient azureDevOps;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=======================================");
+            Console.WriteLine("==  Azure DevOps BuidID Incrementer  ==");
+            Console.WriteLine("=======================================");
+
+            AskForOrgName();
+            ChooseProject();
+            AskForMaxIncrement();
+            AskForExecution();
+                    
+            Console.WriteLine("finished - press any key to exit");
+            Console.ReadKey();
+        }
+      
+        private static void AskForOrgName()
+        {
+            Console.WriteLine();
+            Console.WriteLine(@"Please enter organisation name: (i.e. https://dev.azure.com/NAME/");
+
+            var name = Console.ReadLine();
+
+            azureDevOps = new AzureDevOpsClient(name);
+
+            Console.WriteLine();
+            Console.WriteLine("Try to connect to " + azureDevOps.Endpoint);
+        }
+        private static void ChooseProject()
+        {
+            Console.WriteLine();
+            var projects = azureDevOps.Projects;
+            int choosenProject = 0;
+            string line = null;
+            do
+            {
+                Console.WriteLine("Please choose a project:");
+                var i = 1;
+                foreach (var pro in projects)
+                {
+                    Console.WriteLine(i + " " + pro.Name);
+                    i++;
+                }
+                line = Console.ReadLine();
+            } while (String.IsNullOrWhiteSpace(line) || int.TryParse(line, out choosenProject) == false || choosenProject <= 0 || choosenProject > projects.Count);
+
+
+            azureDevOps.Select(choosenProject - 1);
+            Console.WriteLine("Choosen project is " + azureDevOps.SelectedProject.Name);
+        }
+        private static void AskForMaxIncrement()
+        {
+            Console.WriteLine();
+
+            var lastId = azureDevOps.LastBuildIdWithOutDeleted;
+            string line;
+            int maxId = lastId;
+            Console.WriteLine($"Last BuildId is currently {lastId}");
+            do
+            {
+                Console.WriteLine("Increment until:");
+                line = Console.ReadLine();
+            } while (String.IsNullOrWhiteSpace(line) || int.TryParse(line, out maxId) == false || maxId <= lastId);
+            Console.WriteLine($"You choose to increment until " + maxId);
+            azureDevOps.SetMaxBuildId(maxId);
+        }
+        private static void AskForExecution()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Do you really want to execute");
+            Console.WriteLine("press Y to continue");
+            var key = Console.ReadKey(true);
+
+            if (key.KeyChar == 'y' || key.KeyChar == 'Y')
+            {
+
+                //Console.WriteLine("DONE");
+                Console.WriteLine();
+
+                azureDevOps.CloneBuildDefinitionIfNeeded();
+                Console.WriteLine("Cloned first definition found");
+
+                var ids = azureDevOps.MakeDummyIncrementBuildsUntilMax();
+
+                foreach (int id in ids)
+                {
+                    Console.WriteLine($"Created dummy build with id {id} (Queue -> Cancelled -> Deleted)");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Do you want to delete the dummy build definition");
+                Console.WriteLine("press Y to continue");
+                if (key.KeyChar == 'y' || key.KeyChar == 'Y')
+                {
+                    azureDevOps.DeleteBuildDefinitinIfPossible();
+                    Console.WriteLine("Cloned build definition deleled");
+                }
+            }
+        }
+
+
+    }
+}
